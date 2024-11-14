ed0a0e2783e4011e6027d60449747cb9246faf65
Ismailbak <Ismail.bakraoui0@gmail.com>
2024-11-14 15:13:15 +0100

    first commit

---
+++ bibliotheque/urls.py
@@ -0,0 +1,8 @@
+# bibliotheque/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('livres/', views.liste_livres, name='liste_livres'),
+    path('livres/<int:id>/', views.detail_livre, name='detail_livre'),
+]
